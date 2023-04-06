9b254b82d4b97cc0ce301619c3f47c28fea3227b
Suvrajeet Banerjee <sjb.blackhat@gmail.com>
2023-04-06 15:31:38 +0000

    upgradeable-contracts

---
+++ 10.upgradeable-contracts/hardhat.config.js
@@ -0,0 +1,53 @@
+require("@openzeppelin/hardhat-upgrades");
+require("@nomicfoundation/hardhat-toolbox");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.17",
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// require("@nomicfoundation/hardhat-toolbox");
+
+// /** @type import('hardhat/config').HardhatUserConfig */
+// module.exports = {
+//   solidity: "0.8.18",
+// };
